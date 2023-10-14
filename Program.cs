a1176f9513daf29790e7bc5e0ee47ec58524c97f
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-10-14 12:56:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HW_GB_000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(" ");
+            if (input.Length == 3)
+            {
+                int result = 0;
+                int number1 = int.Parse(input[0]);
+                string operation = input[1];
+                int number2 = int.Parse(input[2]);
+                if (operation == "+")
+                {
+                    result = number1 + number2;
+                }
+                if (operation == "-")
+                {
+                    result = number1 - number2;
+                }
+                if (operation == "/")
+                {
+                    result = number1 / number2;
+                }
+                if (operation == "*")
+                {
+                    result = number1 * number2;
+                }
+                Console.WriteLine("Результат выполнения операции (" + operation + ") = " + result);
+            }
+            else
+                Console.WriteLine("Для использования программы введите: число пробел знак(+-/*) пробел число");
+}
+}
+}
